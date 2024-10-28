696933b007309e2a7c5c809b4cb253e821d36602
vandojapan <30983282+vandojapan@users.noreply.github.com>
2024-10-28 19:25:27 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import react from '@vitejs/plugin-react';
 
 declare module "@remix-run/node" {
   interface Future {
@@ -19,10 +20,11 @@ export default defineConfig({
         v3_lazyRouteDiscovery: true,
       },
     }),
+    react(),
     tsconfigPaths(),
   ],
   build: {
-    outDir: 'public/build', // Cloudflare Pagesが期待する出力先
+    outDir: 'public/build',  // Cloudflare Pagesが期待する出力先
     rollupOptions: {
       input: '/app/entry.client.tsx', // クライアントエントリーポイント
     },
