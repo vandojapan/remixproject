8409266156295c76862a316a666e36699c100eb1
vandojapan <30983282+vandojapan@users.noreply.github.com>
2024-10-28 17:47:04 +0900

    Create remix.config.js

---
+++ remix.config.js
@@ -0,0 +1,7 @@
+/** @type {import('@remix-run/dev').AppConfig} */
+module.exports = {
+  appDirectory: "app", // アプリのディレクトリ
+  assetsBuildDirectory: "public/build", // 公開されるビルドアセットの出力ディレクトリ
+  serverBuildDirectory: "build", // サーバビルドの出力ディレクトリ
+  // 他の設定も必要に応じて追加するのじゃ
+};
