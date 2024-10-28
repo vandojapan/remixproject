e0b2f2697ba261fc687445ab4bb0f520212bfa30
vandojapan <30983282+vandojapan@users.noreply.github.com>
2024-10-28 19:26:48 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import react from '@vitejs/plugin-react';
 
 declare module "@remix-run/node" {
   interface Future {
@@ -20,7 +19,6 @@ export default defineConfig({
         v3_lazyRouteDiscovery: true,
       },
     }),
-    react(),
     tsconfigPaths(),
   ],
   build: {
