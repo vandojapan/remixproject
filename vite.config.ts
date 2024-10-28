e6baea8da231ca1da02cf54fb189e38f186cc917
vandojapan <30983282+vandojapan@users.noreply.github.com>
2024-10-28 19:08:33 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,16 @@
-import { vitePlugin as remix } from "@remix-run/dev";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
-
-declare module "@remix-run/node" {
-  interface Future {
-    v3_singleFetch: true;
-  }
-}
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [
-    remix({
-      future: {
-        v3_fetcherPersist: true,
-        v3_relativeSplatPath: true,
-        v3_throwAbortReason: true,
-        v3_singleFetch: true,
-        v3_lazyRouteDiscovery: true,
-      },
-    }),
+    react(),
     tsconfigPaths(),
   ],
   build: {
-    outDir: 'public/build', // Cloudflare Pagesが期待する出力先
+    outDir: 'public/build',  // Cloudflare Pagesが期待する出力先
     rollupOptions: {
       input: '/app/entry.client.tsx', // クライアントエントリーポイント
-    },
-  },
-  server: {
-    fs: {
-      allow: ['.'], // ローカルファイルシステムからの読み込みを許可
     },
   },
 });
