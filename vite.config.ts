9dbcde49adf234a2fd397030d3417e69be66df04
vandojapan <30983282+vandojapan@users.noreply.github.com>
2024-10-28 19:17:02 +0900

    Revert "Update vite.config.ts"
    This reverts commit e6baea8da231ca1da02cf54fb189e38f186cc917.

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,35 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import { vitePlugin as remix } from "@remix-run/dev";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+declare module "@remix-run/node" {
+  interface Future {
+    v3_singleFetch: true;
+  }
+}
 
 export default defineConfig({
   plugins: [
-    react(),
+    remix({
+      future: {
+        v3_fetcherPersist: true,
+        v3_relativeSplatPath: true,
+        v3_throwAbortReason: true,
+        v3_singleFetch: true,
+        v3_lazyRouteDiscovery: true,
+      },
+    }),
     tsconfigPaths(),
   ],
   build: {
-    outDir: 'public/build',  // Cloudflare Pagesが期待する出力先
+    outDir: 'public/build', // Cloudflare Pagesが期待する出力先
     rollupOptions: {
       input: '/app/entry.client.tsx', // クライアントエントリーポイント
+    },
+  },
+  server: {
+    fs: {
+      allow: ['.'], // ローカルファイルシステムからの読み込みを許可
     },
   },
 });
