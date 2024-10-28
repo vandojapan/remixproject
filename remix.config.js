b347af8ab6c2f37f5c4c209e24856754b5b52469
vandojapan <30983282+vandojapan@users.noreply.github.com>
2024-10-28 18:45:40 +0900

    Delete remix.config.js

--- remix.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('@remix-run/dev').AppConfig} */
-module.exports = {
-  serverBuildTarget: "cloudflare-pages",
-  appDirectory: "app",
-  assetsBuildDirectory: "build/client",
-  publicPath: "/build/client/",
-  serverBuildPath: "build/server.js",
-};
